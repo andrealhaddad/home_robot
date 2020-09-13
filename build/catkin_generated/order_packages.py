8ebd6e092d7df68eaa93ef4d4fc23fa4e3fb8aaa
andrealhaddad <andre.alhaddad@gmail.com>
2020-09-13 14:18:29 +0000

    updated navigation and planning, pick/drop objects ready, markers showing objects

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/workspace/home_robot/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/workspace/home_robot/devel;/opt/ros/kinetic".split(';') if "/home/workspace/home_robot/devel;/opt/ros/kinetic" != "" else []
